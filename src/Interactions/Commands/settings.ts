a00f9cb998e703f32322bf2a0a071bf63d6c392d
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-04-05 22:43:12 +0100

    refactor: edit subcommand

--- src/Interactions/Commands/settings.ts
+++ src/Interactions/Commands/settings.ts
@@ -29,58 +29,41 @@ module.exports = {
 		} else if (subCommand === 'edit') {
 			const setting = interaction.options.getString('setting');
 			const value = interaction.options.getString('value');
-			if (setting === 'webhook') {
-				if (!value!.includes('/api/webhooks')) return await interaction.editReply({
-					embeds: [
-						client.utils.defaultEmbed(client, client.Colors.RED)
-							.setDescription('Invalid value, your webhook should look something like: `https://discord.com/api/webhooks/NUMBER/a-zA-Z0-9`')
-					]
-				});
-				const isGuildInDatabase: any = await client.database.query('SELECT * FROM settings WHERE guildID = ?', interaction.guild!.id);
-				if (!isGuildInDatabase.length) {
-					await client.database.query('INSERT INTO settings (guildID, webhookURL) VALUES (?, ?)', interaction.guild!.id, AES.encrypt(value!, client.config.CIPHER_KEY).toString());
-					return await interaction.editReply({
-						embeds: [
-							client.utils.defaultEmbed(client, client.Colors.GREEN)
-								.setDescription(`Successfully updated ${setting}`)
-						]
-					});
-				}
-				await client.database.query('UPDATE settings SET webhookURL = ? WHERE guildID = ?', AES.encrypt(value!, client.config.CIPHER_KEY).toString(), interaction.guild!.id);
-				return await interaction.editReply({
-					embeds: [
-						client.utils.defaultEmbed(client, client.Colors.GREEN)
-							.setDescription(`Successfully updated ${setting}`)
-					]
-				});
 
-			} else if (setting === 'emoji') {
-				if (!['custom', 'default'].includes(value!)) {
-					return await interaction.editReply({
-						embeds: [
-							client.utils.defaultEmbed(client, client.Colors.RED)
-								.setDescription('Invalid value, your edit emoji value you be either: `default` or `custom`.')
-						]
-					});
-				}
-				const isGuildInDatabase: any = await client.database.query('SELECT * FROM settings WHERE guildID = ?', interaction.guild!.id);
-				if (!isGuildInDatabase.length) {
-					await client.database.query('INSERT INTO settings (guildID, emoji) VALUES (?, ?)', interaction.guild!.id, value!);
-					return await interaction.editReply({
-						embeds: [
-							client.utils.defaultEmbed(client, client.Colors.GREEN)
-								.setDescription(`Successfully updated ${setting}`)
-						]
-					});
-				}
-				await client.database.query('UPDATE settings SET emoji = ? WHERE guildID = ?', value!, interaction.guild!.id);
-				return await interaction.editReply({
+			if (setting === 'webhook' && !value!.includes('/api/webhooks')) return await interaction.editReply({
+				embeds: [
+					client.utils.defaultEmbed(client, client.Colors.RED)
+						.setDescription('Invalid value, your webhook should look something like: `https://discord.com/api/webhooks/ID/a-zA-Z0-9`')
+				]
+			});
+			if (setting === 'emoji' && !['custom', 'default'].includes(value!)) return await interaction.editReply({
+				embeds: [
+					client.utils.defaultEmbed(client, client.Colors.RED)
+						.setDescription('Invalid value, your edit emoji value can either be `custom` or `default`')
+				]
+			});
+
+			const isGuildInDatabase: any = await client.database.query('SELECT * FROM settings WHERE guildID = ?', interaction.guild!.id);
+			if (!isGuildInDatabase.length) {
+				await client.database.query(`INSERT INTO settings (guildID, ${setting === 'webhook' ? 'webhookURL' : 'emoji'}) VALUES (?, ?)`,
+					interaction.guild!.id,
+					setting === 'webhook' ? AES.encrypt(value!, client.config.CIPHER_KEY).toString() : value!);
+				return interaction.editReply({
 					embeds: [
 						client.utils.defaultEmbed(client, client.Colors.GREEN)
 							.setDescription(`Successfully updated ${setting}`)
 					]
 				});
 			}
+			await client.database.query(`UPDATE settings SET ${setting === 'webhook' ? 'webhookURL' : 'emoji'} = ? WHERE guildID = ?`, 
+				setting === 'webhook' ? AES.encrypt(value!, client.config.CIPHER_KEY).toString() : value!,
+				interaction.guild!.id);
+			return interaction.editReply({
+				embeds: [
+					client.utils.defaultEmbed(client, client.Colors.GREEN)
+						.setDescription(`Successfully updated ${setting}`)
+				]
+			});
 		}
 	}
 };
