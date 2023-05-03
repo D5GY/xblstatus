298108441045dc1b2c746e6518f4f221845c1b84
Jinx <52132553+Dev-Jinx@users.noreply.github.com>
2023-05-03 15:41:23 +0100

    refactor: mysql connect & query

--- src/Utils/database.ts
+++ src/Utils/database.ts
@@ -1,53 +1,29 @@
-import { Connection, createConnection } from 'mysql2';
+import { Connection, createConnection } from 'mysql';
 import { Config } from './config';
 
 export class Database extends null {
 	private static _database: Connection;
-	private static _keepAlive: NodeJS.Timeout | null = null;
-	private static _isConnected: boolean = false;
 
-	public static connect() {
-		if (this._isConnected) return;
-		return new Promise<void>((promise, reject) => {
-			this._database = createConnection({
-				host: Config.DATABASE.HOST,
-				user: Config.DATABASE.USER,
-				password: Config.DATABASE.PASSWORD,
-				database: Config.DATABASE.DATABASE,
-				port: Config.DATABASE.PORT
-			});
-			if (!this._isConnected) this._database.connect(error => {
-				if (error) return reject(error);
-				this._isConnected = true;
-				console.log('DATABASE: Connected');
-				this._keepAlive = setInterval(() => {
-					if (!this._isConnected) {
-						clearInterval(this._keepAlive!);
-						this._keepAlive = null;
-						this.connect();
-						return;
-					}
-					console.log('DATABASE: 30mins keep alive query');
-					this.query('SELECT 1 + 1').catch(error => {
-						console.error(error);
-					});
-				}, 1.8e+6);
-				console.log('DATABASE: 30mins keep alive started');
-			});
+	constructor() {
+		Database._database = createConnection({
+			host: Config.DATABASE.HOST,
+			user: Config.DATABASE.USER,
+			password: Config.DATABASE.PASSWORD,
+			database: Config.DATABASE.DATABASE,
+			port: Config.DATABASE.PORT
 		});
 	}
-	public static query(sql: string, ...args: unknown[]) {
-		if (!this._isConnected) return Promise.reject(new Error('Database not connected'));
-		return new Promise((resolve, reject) => {
-			try {
-				this._database.query(sql, args, (error, results) => {
-					if (error)
-						return reject(error);
-					resolve(results);
-				});
-			} catch (error) {
-				reject(error);
-			}
-		});
+
+	public static connect(): Promise<unknown> {
+		return new Promise((resolve, reject) => Database._database.connect(error => {
+			if (error) reject;
+			else resolve(this);
+		}));
+	}
+	public static query<T = unknown>(sql: string, ...args: unknown[]): Promise<T[]> {
+		return new Promise((resolve, reject) => this._database.query(sql, args, (error, results) => {
+			if (error) reject(error);
+			else resolve(results);
+		}));
 	}
 }
